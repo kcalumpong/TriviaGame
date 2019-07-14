7c6dc262f12716678160fc62a0bd2cc121d95026
Kristina Calumpong <kcalumpong@gmail.com>
2019-07-13 20:02:51 -0700

    added functioning timer, created function for radio buttons, removed if/else then statement, changed color of timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,10 @@
 var userCorrect = 0;
 var userIncorrect = 0;
-var minutes = 10;
+var minutes = 2;
+var seconds = 60;
 var intervalId;
-var unanswered = 0;
+var unanswered = 5;
+var b = false;
 
 
 $(document).ready(function() {
@@ -19,96 +21,76 @@ $("#start").click(function(){
     }
 
     function decrement () {
-        minutes--;
-        $("#timeRemaining").html("<h3> Time Remaining: " + minutes + "</h3>")
         
-        if (minutes === 0) {
+
+        if (seconds === 0 && minutes === 0) {
             stop();
+            return;
         }
+
+        if (seconds === 0) {
+            minutes--;
+            $("#minute").text(minutes);
+            seconds = 60;
+        }
+    
+        seconds--;
+        $("#seconds").text(seconds);
+        if (b === false) {// should only hit once
+            $('#minute').text(2);
+            b = true;
+        }        
     }
     run()  
 });
 
 $("#finalScore").hide();
 
-$("#finishedButton").click(function(){
-    $(this).hide();
-    $("#questions").hide();
-    $("#finalScore").show();
-});
-
-  $("#questionOne").click(function() {
-  var answerOne = $('input[type="radio"][name="country"]:checked').val();
-  console.log(answerOne);
-
-  if (answerOne === "4") {
-      userCorrect++;
-  } else {
-      userIncorrect++;
-  }
-  scoreBoard();
-
-  });
-
-  $("#questionTwo").click(function() {
-    var answerTwo = $('input[type="radio"][name="drink"]:checked').val();
-    console.log(answerTwo);
-  
-    if (answerTwo === "3") {
+function determineScore(userAnswer, solution) {
+    if (!userAnswer) return;
+    if (userAnswer === solution) {
         userCorrect++;
+        unanswered--;
+
     } else {
         userIncorrect++;
+        unanswered--;
+
     }
-    scoreBoard();
-  
-    });
+}
 
-    $("#questionThree").click(function() {
-        var answerThree = $('input[type="radio"][name="dessert"]:checked').val();
-        console.log(answerThree)
+$("#finishedButton").click(function(){
+    var answerOne = $('input[type="radio"][name="country"]:checked').val();
+    var answerTwo = $('input[type="radio"][name="drink"]:checked').val();
+    var answerThree = $('input[type="radio"][name="dessert"]:checked').val();
+    var answerFour = $('input[type="radio"][name="got"]:checked').val();
+    var answerFive = $('input[type="radio"][name="app"]:checked').val();
 
-        if (answerThree === "4") {
-            userCorrect++;
-        } else {
-            userIncorrect++;
-        }
-        scoreBoard();
+    determineScore(parseInt(answerOne), 4);
+    determineScore(parseInt(answerTwo), 3);
+    determineScore(parseInt(answerThree), 4);
+    determineScore(parseInt(answerFour), 1);
+    determineScore(parseInt(answerFive), 2);
+    stop();
 
-    })
+});
 
-    $("#questionFour").click(function() {
-        var answerFour = $('input[type="radio"][name="got"]:checked').val();
-        console.log(answerFour)
-
-        if (answerFour === "1") {
-            userCorrect++;
-        } else {
-            userIncorrect++;
-        }
-        scoreBoard();
-    })
-
-    $("#questionFive").click(function(){
-        var answerFive = $('input[type="radio"][name="app"]:checked').val();
-        console.log(answerFive);
-
-        if (answerFive === "2") {
-            userCorrect++;
-        } else {
-            userIncorrect++;      
-        }
-        scoreBoard();
-       
-    })
+    function hideShowScoreboard() {
+        $("#questions").hide();
+        $("#finalScore").show();
+        $("#finishedButton").hide();
+    }
 
     function stop() {
         clearInterval(intervalId)
         scoreBoard();
+        hideShowScoreboard();
     }
 
     var scoreBoard = function() {
         $("#userCorrect").text("Correct Answers: " + userCorrect)
         $("#userIncorrect").text("Incorrect Answers: " + userIncorrect)
+        $("#unanswered").text("Unanswered: " + unanswered);
     }
 
  
