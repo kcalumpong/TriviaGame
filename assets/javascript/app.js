87ed19ea2a57e722a9c226450949077f846e3535
Kristina Calumpong <kcalumpong@gmail.com>
2019-07-14 15:47:27 -0700

    changed unanswered number from 3 to 5

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,81 +7,81 @@ var unanswered = 5;
 var b = false;
 
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-$("#questions").hide();  
+    $("#questions").hide();
 
-$("#start").click(function(){
-    $(this).hide();
-    $("#questions").show();
+    $("#start").click(function () {
+        $(this).hide();
+        $("#questions").show();
 
-    function run() {
-        clearInterval(intervalId);
-        intervalId = setInterval(decrement, 1000);
-    }
-
-    function decrement () {
-        
-
-        if (seconds === 0 && minutes === 0) {
-            stop();
-            return;
+        function run() {
+            clearInterval(intervalId);
+            intervalId = setInterval(decrement, 1000);
         }
 
-        
-        if (seconds === 0) {
-            minutes--;
-            $("#minute").text(minutes);
-            seconds = 60;
-        }
+        function decrement() {
 
-        
-        seconds--;
-        $("#seconds").text(seconds);
-        if (b === false) {// should only hit once
-            $('#minute').text(2);
-            b = true;
-        }
 
-        if (seconds < 10) {
-            $("#seconds").text("0" + seconds)
-        } else {
+            if (seconds === 0 && minutes === 0) {
+                stop();
+                return;
+            }
+
+
+            if (seconds === 0) {
+                minutes--;
+                $("#minute").text(minutes);
+                seconds = 60;
+            }
+
+
+            seconds--;
             $("#seconds").text(seconds);
+            if (b === false) {// should only hit once
+                $('#minute').text(2);
+                b = true;
+            }
+
+            if (seconds < 10) {
+                $("#seconds").text("0" + seconds)
+            } else {
+                $("#seconds").text(seconds);
+            }
+        }
+        run()
+    });
+
+    $("#finalScore").hide();
+
+    function determineScore(userAnswer, solution) {
+        if (!userAnswer) return;
+        if (userAnswer === solution) {
+            userCorrect++;
+            unanswered--;
+
+        } else {
+            userIncorrect++;
+            unanswered--;
+
         }
     }
-    run()  
-});
 
-$("#finalScore").hide();
+    $("#finishedButton").click(function () {
+        var answerOne = $('input[type="radio"][name="country"]:checked').val();
+        var answerTwo = $('input[type="radio"][name="drink"]:checked').val();
+        var answerThree = $('input[type="radio"][name="dessert"]:checked').val();
+        var answerFour = $('input[type="radio"][name="got"]:checked').val();
+        var answerFive = $('input[type="radio"][name="app"]:checked').val();
 
-function determineScore(userAnswer, solution) {
-    if (!userAnswer) return;
-    if (userAnswer === solution) {
-        userCorrect++;
-        unanswered--;
+        determineScore(parseInt(answerOne), 4);
+        determineScore(parseInt(answerTwo), 3);
+        determineScore(parseInt(answerThree), 4);
+        determineScore(parseInt(answerFour), 1);
+        determineScore(parseInt(answerFive), 2);
+        stop();
 
-    } else {
-        userIncorrect++;
-        unanswered--;
-
-    }
-}
-
-$("#finishedButton").click(function(){
-    var answerOne = $('input[type="radio"][name="country"]:checked').val();
-    var answerTwo = $('input[type="radio"][name="drink"]:checked').val();
-    var answerThree = $('input[type="radio"][name="dessert"]:checked').val();
-    var answerFour = $('input[type="radio"][name="got"]:checked').val();
-    var answerFive = $('input[type="radio"][name="app"]:checked').val();
-
-    determineScore(parseInt(answerOne), 4);
-    determineScore(parseInt(answerTwo), 3);
-    determineScore(parseInt(answerThree), 4);
-    determineScore(parseInt(answerFour), 1);
-    determineScore(parseInt(answerFive), 2);
-    stop();
-
-});
+    });
 
     function hideShowScoreboard() {
         $("#questions").hide();
@@ -95,13 +95,13 @@ $("#finishedButton").click(function(){
         hideShowScoreboard();
     }
 
-    var scoreBoard = function() {
+    var scoreBoard = function () {
         $("#userCorrect").text("Correct Answers: " + userCorrect)
         $("#userIncorrect").text("Incorrect Answers: " + userIncorrect)
         $("#unanswered").text("Unanswered: " + unanswered);
     }
 
- 
+
 
 });
 
